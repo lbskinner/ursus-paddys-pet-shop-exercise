aa7790fa377e1f1451d1570e7d1572f3f6698197
Myron <dubtwee@gmail.com>
2020-03-18 14:57:38 -0500

    adding the initial inventory for the pet shop

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,34 @@
 console.log('Paddy\'s Pet Shop');
 
-const inventory = [];
+const inventory = [
+  {
+    name: 'Niffler',
+    type: 'domestic',
+    price: 550,
+    notes: 'Has a tendency to get into trouble pilfering valuables from anywhere and anyone it lays its eyes on.'
+  },
+  {
+    name: 'Murtlap',
+    type: 'wild',
+    price: 250,
+    notes: 'Fleshy rodent like creature. Beware it\'s bite because it may cause hallucinations.'
+  },
+  {
+    name: 'Billywig',
+    type: 'wild',
+    price: 148,
+    notes: 'When stung the person will suffer from a giddiness and eventually spontaneous floating.'
+  },
+  {
+    name: 'Graphorns',
+    type: 'mounted',
+    price: 1456,
+    notes: 'Has a tough hide said to be more durable than that of a dragon.'
+  },
+  {
+    name: 'Thunderbird',
+    type: 'combat',
+    price: 5230,
+    notes: 'Said to be able to sense danger from far off and can wipe the memories of extremely large groups of people.'
+  },
+];
