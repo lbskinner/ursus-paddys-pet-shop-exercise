49b7823a947cf7e82a610b5ba7c3c40d0ec117b8
Leila Skinner <beileiwang@gmail.com>
2020-03-20 11:27:13 -0500

    remove selected pet from inventory to checkout array

--- scripts/client.js
+++ scripts/client.js
@@ -38,31 +38,37 @@ const inventory = [
   }
 ];
 
+const creaturesToCheckout = [];
+
 function init() {
   console.log("READY");
   // display inventory on page when page loads
   render();
   // add event listener for add a new pet
   $(".js-add-new-pet-form").on("submit", addNewPet);
+  // add event listener for purchase button
+  $(".js-pet-card-container").on("click", ".js-btn-purchase", selectCreature);
 }
 
 function render() {
   // render the page inventory to page on initial page load
   $(".js-pet-card-container").empty();
-  for (let pet of inventory) {
-    let petPrice = pet.price.toLocaleString(undefined, {
+  for (let i = 0; i < inventory.length; i++) {
+    // format the price
+    let petPrice = inventory[i].price.toLocaleString(undefined, {
       minimumFractionDigits: 2
     });
+    // display the inventory on page
     $(".js-pet-card-container").append(`
       <div class="col-4 mb-3 px-2">
 
         <div class="card">
           <h5 class="card-header">$${petPrice}</h5>
           <div class="card-body">
-            <h5 class="card-title">${pet.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${pet.type}</h6>
-            <p class="card-text">${pet.notes}</p>
-            <button class="btn btn-primary btn-block">Purchase</button>
+            <h5 class="card-title">${inventory[i].name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${inventory[i].type}</h6>
+            <p class="card-text">${inventory[i].notes}</p>
+            <button class="btn btn-primary btn-block js-btn-purchase" data-index=${i}>Purchase</button>
           </div>
         </div>
 
@@ -86,4 +92,35 @@ function addNewPet(event) {
   render();
   // log inventory in the console to see if it matches the DOM
   console.log(inventory);
+}
+
+function selectCreature() {
+  console.log("SELECT CREATURE");
+  // log the index
+  console.log($(this).data("index"));
+  let petIndex = $(this).data("index");
+  // delete the selected pet from inventory
+  let selectPet = inventory.splice(petIndex, 1);
+  // add the deleted pet to new global array for checkout
+  creaturesToCheckout.push(...selectPet);
+  // empty the checkout ul
+  $(".js-select-list").empty();
+  // render the selected pets on page
+  let totalPrice = 0;
+  for (let pet in creaturesToCheckout) {
+    totalPrice += pet.price;
+    // format the price
+    // let petPrice = pet.price.toLocaleString(undefined, {
+    //   minimumFractionDigits: 2
+    // });
+    // display selected pets on page
+    $(".js-select-list").append(`
+    <li class="list-group-item">${pet.name} - $${pet.price}</li>
+    `);
+  }
+  // add total price at the end
+  $(".js-select-list").append(`
+  <li class="list-group-item list-group-item-success">Total Price - $${totalPrice}</li>
+    `);
+  render();
 }
